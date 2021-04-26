4c8c4503c4aa3c7fdffbea0e184e6391045c0c70
EC2 Default User <ec2-user@ip-172-31-93-119.ec2.internal>
2021-04-26 20:21:51 +0000

    Changes to Test

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,10 +1,12 @@
 import json
 import pytest
-from helloworld.application import create_app as application
+from helloworld.app import create_app
 
-@pytest.fixture
+config_name = os.getenv('FLASK_CONFIG')
+app = create_app(config_name)
+
 def client():
-    return application.test_client()
+    return create_app.test_client()
 
 def test_response(client):
     return "The Website Works Successfully"
