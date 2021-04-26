adec0d7085df52f5cb92450ce5b68e8ba1db1307
EC2 Default User <ec2-user@ip-172-31-93-119.ec2.internal>
2021-04-26 20:59:23 +0000

    Change in Project Directory

---
+++ application.py
@@ -0,0 +1,9 @@
+import os
+
+from app import create_app
+
+config_name = os.getenv('FLASK_CONFIG')
+application = create_app(config_name)
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0',port=8080)
