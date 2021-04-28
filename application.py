7df3251d3d4deae35ef558f31acb59e5385f0025
EC2 Default User <ec2-user@ip-172-31-93-119.ec2.internal>
2021-04-28 01:21:35 +0000

    changes in Enviroment

---
+++ application.py
@@ -0,0 +1,32 @@
+# run.py
+
+import os
+
+from app import create_app
+
+config_name = os.getenv('FLASK_CONFIG')
+app = create_app(config_name)
+
+if __name__ == '__main__':
+    default_port = "80"
+    default_host = "0.0.0.0"
+    parser = optparse.OptionParser()
+    parser.add_option("-H", "--host",
+                      help=f"Hostname of Flask app {default_host}.",
+                      default=default_host)
+
+    parser.add_option("-P", "--port",
+                      help=f"Port for Flask app {default_port}.",
+                      default=default_port)
+
+    parser.add_option("-d", "--debug",
+                      action="store_true", dest="debug",
+                      help=optparse.SUPPRESS_HELP)
+
+    options, _ = parser.parse_args()
+
+    application.run(
+        debug=options.debug,
+        host=options.host,
+        port=int(options.port)
+    )
