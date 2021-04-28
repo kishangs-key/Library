b6eda924ffa94b70e1c893a1ac5971cff8d781dd
EC2 Default User <ec2-user@ip-172-31-93-119.ec2.internal>
2021-04-28 02:01:37 +0000

    Changes to helloworld

--- application.py
+++ application.py
@@ -1,32 +1,10 @@
 # run.py
-
 import os
 
-from app import create_app
+from app import create_app as application
 
 config_name = os.getenv('FLASK_CONFIG')
-app = create_app(config_name)
+app = application(config_name)
 
 if __name__ == '__main__':
-    default_port = "80"
-    default_host = "0.0.0.0"
-    parser = optparse.OptionParser()
-    parser.add_option("-H", "--host",
-                      help=f"Hostname of Flask app {default_host}.",
-                      default=default_host)
-
-    parser.add_option("-P", "--port",
-                      help=f"Port for Flask app {default_port}.",
-                      default=default_port)
-
-    parser.add_option("-d", "--debug",
-                      action="store_true", dest="debug",
-                      help=optparse.SUPPRESS_HELP)
-
-    options, _ = parser.parse_args()
-
-    application.run(
-        debug=options.debug,
-        host=options.host,
-        port=int(options.port)
-    )
+    app.run(host='0.0.0.0',port=8080)
