dd32e719e12791c672974e5efbc9f39994f03b35
EC2 Default User <ec2-user@ip-172-31-93-119.ec2.internal>
2021-04-28 02:16:33 +0000

    Change to application

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import os
 from app import create_app as application
 
 config_name = os.getenv('FLASK_CONFIG')
-app = application(config_name)
+application = application(config_name)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    application.run(host='0.0.0.0',port=8080)
