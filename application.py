f620b418b11b5bf892862e7fb9c2cd8a18bcef2b
EC2 Default User <ec2-user@ip-172-31-93-119.ec2.internal>
2021-04-28 03:14:18 +0000

    Change to Application.py

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import os
 from app import application
 
 config_name = 'development'
-app = application(config_name)
+application = application(config_name)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    application.run(host='0.0.0.0',port=8080)
