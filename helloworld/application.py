48938cc006ae15a3892593ba8f967fcb34bc99e3
EC2 Default User <ec2-user@ip-172-31-93-119.ec2.internal>
2021-04-28 03:26:52 +0000

    Final Change

--- helloworld/application.py
+++ helloworld/application.py
@@ -6,4 +6,4 @@ config_name = 'development'
 application = application(config_name)
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0',port=8080)
+    application.run(host='0.0.0.0',port=8080,debug=True)
